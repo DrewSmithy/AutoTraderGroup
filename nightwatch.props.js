5a56af56686bbd40691b824d02136e189969d0cb
atrain smith <andrew.smithno2@gmail.com>
2018-06-06 08:57:45 -0600

    Initial Commit

---
+++ nightwatch.props.js
@@ -0,0 +1,5 @@
+module.exports = {
+    resourcePath: "C:\\Users\\atrai\\DevMountainClassWork\\Testing-Resources\\",
+    seleniumServer: "selenium-server-standalone-3.12.0.jar",
+    chromedriver: "chromedriver.exe"
+ }
