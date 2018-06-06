15ffd878a356120af8b4deeafef9731ca5ad8b42
atrain smith <andrew.smithno2@gmail.com>
2018-06-06 09:48:25 -0600

    Removed some files/folders

--- nightwatch.props.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    resourcePath: "C:\\Users\\atrai\\DevMountainClassWork\\Testing-Resources\\",
-    seleniumServer: "selenium-server-standalone-3.12.0.jar",
-    chromedriver: "chromedriver.exe"
- }
