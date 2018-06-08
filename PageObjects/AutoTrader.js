44103b2b981f61b5b7425d23618dbb6893cb4158
atrain smith <andrew.smithno2@gmail.com>
2018-06-08 10:39:33 -0600

    Final Functions

--- PageObjects/AutoTrader.js
+++ PageObjects/AutoTrader.js
@@ -1,11 +1,20 @@
+var autoTraderCommands = {
+    getMake1: (option) => `select[id="make-1"] option[value="${option}"]`,
+    getModel1: (option) => `select[id="model-1"] option[value="${option}"]`,
+    getMake2: (option) => `select[id="make-2"] option[value="${option}"]`,
+    getModel2: (option) => `select[id="model-2"] option[value="${option}"]`,
+    getMake3: (option) => `select[id="make-3"] option[value="${option}"]`,
+    getModel3: (option) => `select[id="model-3"] option[value="${option}"]`
+}
+
+
 module.exports = {
 
     url: 'https://www.autotrader.com/',
-
+    commands: [autoTraderCommands],
     elements: {
-
         emailInput: {
-            selector: ('(//input[@name="username"])[1]'),
+            selector: '(//input[@name="username"])[1]',
             locateStrategy: 'xpath'
         },
         passInput: {
@@ -16,9 +25,86 @@ module.exports = {
             selector: ('(//input[@type="submit"])[1]'),
             locateStrategy: 'xpath'
         },
-        signinButton: ('span[class="hidden-xs margin-horizontal-sm text-link"]'),
-        viewAccount: ('a[class="hidden-xs margin-horizontal-sm"]')
-
-
+        signinButton: {
+            selector: 'span[class="hidden-xs margin-horizontal-sm text-link"]',
+            locateStrategy: 'css selector'
+        },
+        viewAccount: {
+            selector: ('a[class="hidden-xs margin-horizontal-sm"]'),
+            locateStrategy: 'css selector'
+        },
+        dropDown: {
+            selector: ('a[class="dropdown-toggle"]'),
+            locateStrategy: 'css selector'
+        },
+        compareModelsButton: {
+            selector: ('a[href="/research/compare?rdpage=SUBNAV"]'),
+            locateStrategy: 'css selector'
+        },
+        zipInput: {
+            selector: ('input[id="address"]'),
+            locateStrategy: 'css selector'
+        },
+        changeZipButton: {
+            selector: ('//input[@id="change-zip-button"]'),
+            locateStrategy: 'xpath'
+        },
+        make1: {
+            selector: ('select[id="make-1"]'),
+            locateStrategy: 'css selector'
+        },
+        make2: {
+            selector: ('select[id="make-2"]'),
+            locateStrategy: 'css selector'
+        },
+        make3: {
+            selector: ('select[id="make-3"]'),
+            locateStrategy: 'css selector'
+        },
+        model1: {
+            selector: ('select[id="model-1"]'),
+            locateStrategy: 'css selector'
+        },
+        model2: {
+            selector: ('select[id="model-2"]'),
+            locateStrategy: 'css selector'
+        },
+        model3: {
+            selector: ('select[id="model-3"]'),
+            locateStrategy: 'css selector'
+        },
+        addModel1: {
+            selector: ('span[id="add-1"]')
+        },
+        addModel2: {
+            selector: ('span[id="add-2"]')
+        },
+        addModel3: {
+            selector: ('span[id="add-3"]')
+        },
+        modelImage1:{
+            selector: ('img[id="main-car-image-1"]'),
+            locateStrategy: 'css selector'
+        },
+        modelImage2: {
+            selector: ('img[id="main-car-image-2"]'),
+            locateStrategy: 'css selector'
+        },
+        modelImage3: {
+            selector: ('img[id="main-car-image-3"]'),
+            locateStrategy: 'css selector'
+        },
+        price1: {
+            selector: ('(//span[@class="price"])[1]'),
+            locateStrategy: 'xpath' 
+        },
+        price2: {
+            selector: ('(//span[@class="price"])[2]'),
+            locateStrategy: 'xpath' 
+        },
+        price3: {
+            selector: ('(//span[@class="price"])[3]'),
+            locateStrategy: 'xpath' 
+        }
     }
 }
--- Tests/Test.js
+++ Tests/Test.js
@@ -1,29 +1,91 @@
 let loginTest = (pageObject, email, password) => {
     pageObject
         .click('@signinButton')
-        .waitForElementPresent('@emailInput', 5000)
+        .waitForElementVisible('@emailInput', 10000)
         .setValue('@emailInput', email)
         .setValue('@passInput', password)
-        .waitForElementVisible('@submitButton', 5000)
+        .waitForElementVisible('@submitButton', 8000, 'Submitting Login')
         .click('@submitButton')
-        .waitForElementPresent('@viewAccount', 5000)
+        .waitForElementPresent('@viewAccount', 8000, 'Login Successful')
         .expect.element('@viewAccount').text.to.equal('View Account')
 
+}
+let compareCars = (comparePage, make1, model1, make2, model2, make3, model3) => {
+    comparePage
+        .click('@dropDown')
+        .click('@compareModelsButton')
+        .waitForElementPresent('@zipInput', 8000, 'Navigating To Compare Page')
+        .clearValue('@zipInput')
+        .setValue('@zipInput', '95838')
+        .verify.valueContains('@zipInput', '95838')
+        .click('@changeZipButton')
+        .waitForElementNotVisible('@changeZipButton', 10000)
+        .api.pause(800)
+    comparePage
+        .click(comparePage.getMake1(make1))
+        .api.pause(800)
+    comparePage
+        .click(comparePage.getModel1(model1))
+        .verify.containsText('@model1', model1, '1st Make and Model Correct')
+        .api.pause(1000)
+    comparePage
+        .click('@addModel1')
+        .waitForElementPresent('@modelImage1', 8000, '1st Compare Complete')
+        .waitForElementPresent('@make2', 8000)
+        .click(comparePage.getMake2(make2))
+        .api.pause(1000)
+    comparePage
+        .click(comparePage.getModel2(model2))
+        .verify.containsText('@model2', model2, '2nd Make and Model Correct')
+        .api.pause(800)
+    comparePage
+        .click('@addModel2')
+        .waitForElementPresent('@modelImage2', 8000, '2nd Compare Complete')
+        .waitForElementPresent('@make3', 8000)
+        .api.pause(800)
+    comparePage
+        .click(comparePage.getMake3(make3))
+        .api.pause(1000)
+    comparePage
+        .click(comparePage.getModel3(model3))
+        .verify.containsText('@model3', model3, '3rd Make and Model Correct')
+        .api.pause(1000)
+    comparePage
+        .click('@addModel3')
+        .waitForElementPresent('@modelImage3', 8000, '3rd Compare Complete')
+        .waitForElementVisible('@price3', 8000, 'Comparing Done')
+        .api.pause(1000)
+    comparePage
+        .getText('@price1', function (result) {
+            console.log(result.value)
+        })
+        .getText('@price2', function (result) {
+            console.log(result.value)
+        })
+        .getText('@price3', function (result) {
+            console.log(result.value)
+        })
 }
 
 
 module.exports = {
     beforeEach: browser => {
         browser.url('https://www.autotrader.com/')
-            .waitForElementPresent('body', 5000, 'Page Loaded')
+            .waitForElementPresent('body', 8000, 'Page Loaded')
+            .useXpath()
     },
 
     after: browser => {
         browser.end()
     },
 
-    'Logging In': browser => {
-        loginTest(browser.page.AutoTrader(), 'prislocks@gmail.com', 'prislocksunny')
+    // 'Logging In': browser => {
+    //     loginTest(browser.page.AutoTrader(), 'prislocks@gmail.com', 'prislocksunny')
+        
+    // },
+
+    'Comparing 3 Cars': browser => {
+        compareCars(browser.page.AutoTrader(), 'Bentley', 'Continental', 'Honda', 'Civic', 'Nissan', 'GT-R')
     }
 
 }
